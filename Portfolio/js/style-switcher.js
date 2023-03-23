a87966b72700bc86a581dd70257f35122be0d096
Brad Seftel <webdev@brad-seftel.com>
2023-03-23 08:38:28 -0230

    added style switcher

---
+++ Portfolio/js/style-switcher.js
@@ -0,0 +1,4 @@
+const styleSwitcherToggler = document.querySelector = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
